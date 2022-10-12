e34f70afd3b97b6a3d6a0ac26dd889d86758ee01
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-10-12 02:19:15 -0600

    env correction

--- sanity.ts
+++ sanity.ts
@@ -6,7 +6,7 @@ export const config = {
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
 	apiVersion: '2022-03-10',
 	useCdn: process.env.NODE_ENV === 'production', //true
-	token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+	token: process.env.NEXT_PRIVATE_SANITY_TOKEN,
 };
 
 // Set up the client for fetching data in the getProps page functions
